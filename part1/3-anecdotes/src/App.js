5205fb44d8b75d7d89b517baaa1c0c5b5d6e155f
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2022-08-19 17:31:19 +0300

    part 1. Done

--- part1/3-anecdotes/src/App.js
+++ part1/3-anecdotes/src/App.js
@@ -1,8 +1,20 @@
 import { useState } from 'react'
 
-const Button = handleClick => <button onClick={handleClick}>new anecdote</button>
+const Display = ({ anecdote, votes }) => {
+  return (
+    <div>
+      <p>{anecdote}</p>
+      <p>This anecdote has {votes} votes</p>
+    </div>
+    
+  )
+}
 
-const Display = text => <p>{text}</p>
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -14,21 +26,33 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
-
+   
   const [selected, setSelected] = useState(0)
+  const [votesState, setVoted] = useState(new Array(anecdotes.length).fill(0))
+  const [mostVoted, setMostVoted] = useState(0)
 
-  const randomQuote = () => {
-    const rand = Math.floor(Math.random() * anecdotes.length)
-    setSelected(rand)
+  const anecdoteHandle = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
   }
-  console.log(anecdotes[selected])
+
+  const voteHandle = () => {
+    const copyArr = [...votesState]
+    copyArr[selected] += 1
+    setVoted(copyArr)
+    setMostVoted(copyArr.indexOf(Math.max(...copyArr)))
+  }
+
   return (
     <div>
-      
-      <Display text={anecdotes[selected]} />
-      <Button handleClick={randomQuote} />
+      <h1>Anecdote of the day</h1>
+      <Display anecdote={anecdotes[selected]} votes={votesState[selected]} />
+      <Button handleClick={anecdoteHandle} text='new anecdote' />
+      <Button handleClick={voteHandle} text='vote' />
+      <hr />
+      <h1>Anecdote with most votes</h1>
+      <Display anecdote={anecdotes[mostVoted]} votes={votesState[mostVoted]} />
     </div>
   )
 }
 
-export default App;
+export default App
