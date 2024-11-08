96fbfb71ea898d702b75eb01a82ff624730a1404
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2024-11-08 17:10:27 +0100

    part 8.5 done

---
+++ part8/library/library-backend.js
@@ -0,0 +1,142 @@
+const {ApolloServer} = require('@apollo/server')
+const {startStandaloneServer} = require('@apollo/server/standalone')
+
+let authors = [
+    {
+        name: 'Robert Martin',
+        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+        born: 1952,
+    },
+    {
+        name: 'Martin Fowler',
+        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+        born: 1963
+    },
+    {
+        name: 'Fyodor Dostoevsky',
+        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+        born: 1821
+    },
+    {
+        name: 'Joshua Kerievsky', // birth year not known
+        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+    },
+    {
+        name: 'Sandi Metz', // birth year not known
+        id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+    },
+]
+
+let books = [
+    {
+        title: 'Clean Code',
+        published: 2008,
+        author: 'Robert Martin',
+        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+        genres: ['refactoring']
+    },
+    {
+        title: 'Agile software development',
+        published: 2002,
+        author: 'Robert Martin',
+        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+        genres: ['agile', 'patterns', 'design']
+    },
+    {
+        title: 'Refactoring, edition 2',
+        published: 2018,
+        author: 'Martin Fowler',
+        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+        genres: ['refactoring']
+    },
+    {
+        title: 'Refactoring to patterns',
+        published: 2008,
+        author: 'Joshua Kerievsky',
+        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+        genres: ['refactoring', 'patterns']
+    },
+    {
+        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+        published: 2012,
+        author: 'Sandi Metz',
+        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+        genres: ['refactoring', 'design']
+    },
+    {
+        title: 'Crime and punishment',
+        published: 1866,
+        author: 'Fyodor Dostoevsky',
+        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+        genres: ['classic', 'crime']
+    },
+    {
+        title: 'Demons',
+        published: 1872,
+        author: 'Fyodor Dostoevsky',
+        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+        genres: ['classic', 'revolution']
+    },
+]
+
+
+const typeDefs = `
+    type Author {
+        name: String!
+        bookCount: Int!
+    }
+    
+    type Book {
+        title: String!
+        published: Int!
+        author: String!
+        id: ID!
+        genres: [String!]!
+    }
+
+    type Query {
+        authorCount: Int!
+        bookCount: Int!
+        allBooks(author: String, genre: String): [Book!]!
+        allAuthors: [Author!]!
+    }
+`
+
+const resolvers = {
+    Query: {
+        authorCount: () => authors.length,
+        bookCount: () => books.length,
+        // allBooks: () => books,
+        allBooks: (root, args) => {
+            if (args.author === undefined) {
+                if (args.genre === undefined)
+                    return books
+                else
+                    return books.filter(b => b.genres.includes(args.genre))
+            } else {
+                if (args.genre === undefined)
+                    return books.filter(b => b.author === args.author)
+                else
+                    return books.filter(b => b.genres.includes(args.genre)).filter(b => b.author === args.author)
+            }
+
+        },
+        allAuthors: () => authors
+    },
+
+    Author: {
+        name: (root) => root.name,
+        bookCount: (root) => books.filter(b => b.author === root.name).length
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+startStandaloneServer(server, {
+    listen: {port: 4000},
+}).then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
