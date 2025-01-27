9d0fe27fa75cdbaadd8b2a2b9ffb4a0248c0c07f
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2025-01-27 15:19:07 +0100

    part 9.15 done

---
+++ part9/half_stack/src/components/Header.tsx
@@ -0,0 +1,3 @@
+const Header = ({courseName}: { courseName: string }): JSX.Element => (<h1>{courseName}</h1>);
+
+export default Header;
---
+++ part9/half_stack/src/components/Total.tsx
@@ -0,0 +1,5 @@
+const Total = ({totalExercises}: { totalExercises: number }): JSX.Element => (
+    <p>Number of exercises {totalExercises}</p>
+);
+
+export default Total;
